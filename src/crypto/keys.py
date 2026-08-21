89271659daf27fa25c549523df04d5e0e1a794c4
Vincent <vincent24022002@icloud.com>
2026-08-21 23:01:26 +0200

    specific keys  1 argument (path) will create keys with default name in path

--- src/crypto/keys.py
+++ src/crypto/keys.py
@@ -99,4 +99,35 @@ def neighbor_keys():
             )
         )
 
-neighbor_keys()
+#neighbor_keys()
+
+def specific_keys(path):
+
+    PUBLIC_KEY_PATH = path/f"public_key.pem"
+    PRIVATE_KEY_PATH = path/f"private_key.pem"
+
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=4096
+    )
+
+    public_key = private_key.public_key()
+
+    with open(PRIVATE_KEY_PATH, "wb") as f:
+        f.write(
+            private_key.private_bytes(
+                encoding=serialization.Encoding.PEM,
+                format=serialization.PrivateFormat.PKCS8,
+                encryption_algorithm=serialization.NoEncryption()
+            )
+        )
+
+    with open(PUBLIC_KEY_PATH, "wb") as f:
+        f.write(
+            public_key.public_bytes(
+                encoding=serialization.Encoding.PEM,
+                format=serialization.PublicFormat.SubjectPublicKeyInfo
+            )
+        )
+
+specific_keys(Path.cwd())

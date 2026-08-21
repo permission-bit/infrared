b7163ba92b3ff0e5b900566cb2e596fb3c0b2a0d
Vincent <vincent24022002@icloud.com>
2026-08-21 22:39:31 +0200

    home keys creates private and public key with current time in USER

---
+++ src/crypto/keys.py
@@ -0,0 +1,38 @@
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+from pathlib import Path
+from datetime import datetime
+
+def home_keys():
+
+    current_time = datetime.now()
+
+    path = Path.home()
+
+    PUBLIC_KEY_PATH = path/f"public_key_{current_time}.pem"
+    PRIVATE_KEY_PATH = path/f"private_key_{current_time}.pem"
+
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=4096
+    )
+
+    public_key = private_key.public_key()
+
+    with open(PRIVATE_KEY_PATH, "wb") as f:
+        f.write(
+            private_key.private_bytes(
+                encoding=serialization.Encoding.PEM,
+                format=serialization.PrivateFormat.PKCS8,
+                encryption_algorithm=serialization.NoEncryption()
+            )
+        )
+
+    with open(PUBLIC_KEY_PATH, "wb") as f:
+        f.write(
+            public_key.public_bytes(
+                encoding=serialization.Encoding.PEM,
+                format=serialization.PublicFormat.SubjectPublicKeyInfo
+            )
+        )
+home_keys()

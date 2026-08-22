0d9deca74b5c8ffe7972100e4bc58f002a9c0f83
Vincent <vincent24022002@icloud.com>
2026-08-22 18:05:30 +0200

    install newest version command

--- test_in_test_folder.sh
+++ test_in_test_folder.sh
@@ -1,3 +1,7 @@
-pip install --index-url https://test.pypi.org/simple/ \
-    --extra-index-url https://pypi.org/simple/ \
-    infraredx==0.1.2
+python -m pip install --no-cache-dir \
+  --index-url https://test.pypi.org/simple/ \
+  --extra-index-url https://pypi.org/simple/ \
+  vincisex==0.1.5
+
+# if old version in production
+python -m pip install --upgrade vincisex
